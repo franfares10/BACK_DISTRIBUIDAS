47d1fdaffd1e980c001bfd46881b3965e868be40
lmitelman <lautaromitelman@gmail.com>
2021-06-05 21:03:31 -0300

    cloudinary & base64

--- routes/cloudinary.routes.js
+++ routes/cloudinary.routes.js
@@ -4,5 +4,6 @@ const { validarCampos } = require('../middlewares/validar-campos');
 const {subirDocumentosDigitales} = require("../controllers/cloudinary.controllers")
 const router  = Router();
 
-
 router.post("/uploadCloudinary",[check('base64').notEmpty(),validarCampos],subirDocumentosDigitales)
+
+module.exports = router
